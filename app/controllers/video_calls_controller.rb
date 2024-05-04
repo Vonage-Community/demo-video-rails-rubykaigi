766715e95ac2ee10cbab4a0cfbd070a0ff9e8f9c
superchilled <karl@superchilled.co.uk>
2024-05-04 16:12:08 +0100

    Implementing create action

--- app/controllers/video_calls_controller.rb
+++ app/controllers/video_calls_controller.rb
@@ -1,8 +1,28 @@
 class VideoCallsController < ApplicationController
   def new
+    @video_call = VideoCall.new
   end
 
   def create
+    @video_call = VideoCall.new(
+      application_id: ENV['VONAGE_APPLICATION_ID'],
+      uuid: SecureRandom.uuid,
+      **video_call_param
+    )
+
+    begin
+      vonage_video_session = Vonage.video.create_session
+      @video_call.session_id = vonage_video_session.session_id
+
+      if @video_call.save
+        redirect_to @video_call
+      else
+        render :new, status: :unprocessable_entity
+      end
+    rescue => error
+      logger.info(error)
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -12,5 +32,11 @@ class VideoCallsController < ApplicationController
   end
 
   def invite
+  end
+
+  private
+
+  def video_call_params
+    params.require(:video_call).permit(:name)
   end
 end
