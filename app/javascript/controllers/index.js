c8c077d66f6af8cb67f87e1af6594e432ecd588a
superchilled <karl@superchilled.co.uk>
2024-05-08 15:56:20 +0100

    Updating asset imports for prod

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,12 +2,12 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "/assets/controllers/application"
 
-import HelloController from "./hello_controller"
+import HelloController from "/assets/controllers/hello_controller"
 application.register("hello", HelloController)
 
-import JoinRoomController from "./join_room_controller"
+import JoinRoomController from "/assets/controllers/join_room_controller"
 application.register("join-room", JoinRoomController)
 
 import Dialog from '@stimulus-components/dialog'
