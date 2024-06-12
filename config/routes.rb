d2fedcb8663f6cd78590b28ef90b42370a591f82
Karl Lingiah <karl@superchilled.co.uk>
2024-06-12 09:37:18 +0100

    Adding Homepage and Leave Meeting button (#1)
    * Adding homepage and leave video call button

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "video_calls#new"
+  root "homepage#show"
 
   resources :video_calls, only: [:new, :create, :show], param: :uuid do
     member do
